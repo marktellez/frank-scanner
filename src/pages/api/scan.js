1f65192afb5583d3a54d6a95a7bf9c37c8998db0
Mark Tellez <mark@devmentor.live>
2023-03-28 11:57:31 -0600

    initial commit

---
+++ src/pages/api/scan.js
@@ -0,0 +1,74 @@
+import cheerio from "cheerio";
+
+const nonPages = [
+  "jpg",
+  "jpeg",
+  "png",
+  "gif",
+  "bmp",
+  "svg",
+  "webp",
+  "ico",
+  "mp3",
+  "wav",
+  "ogg",
+  "mp4",
+  "webm",
+  "flv",
+  "avi",
+  "mov",
+  "wmv",
+  "mkv",
+  "js",
+];
+
+export default async function handler(req, res) {
+  function ensureAbsolute(url) {
+    return url.startsWith("http") ? url : `https://${url}`;
+  }
+
+  function findInternalLinks(html, url) {
+    const host = new URL(url).host;
+    const $ = cheerio.load(html);
+    const links = $("a")
+      .map((i, el) => $(el).attr("href"))
+      .get()
+      .filter(
+        (href) =>
+          href && href.includes(host) && !nonPages.includes(href.split(".")[1])
+      );
+
+    return pickTopLinks(links, 5);
+  }
+
+  function pickTopLinks(links, count) {
+    return links.slice(0, count);
+  }
+
+  async function collectPageText(links) {
+    const textPromises = links.map(async (link) => {
+      console.dir(`requesting ${link}`);
+      const response = await fetch(link);
+      const html = await response.text();
+      const $ = cheerio.load(html);
+      return $("body").text();
+    });
+
+    const texts = await Promise.all(textPromises);
+    return texts.join(" ");
+  }
+
+  const { url } = req.query;
+
+  const baseUrl = ensureAbsolute(url);
+  const response = await fetch(baseUrl);
+  const html = await response.text();
+  const links = findInternalLinks(html, baseUrl);
+  const text = await collectPageText(
+    links.map((link) =>
+      link.includes(baseUrl.replace(/https?:\/\//, "")) ? link : baseUrl + link
+    )
+  );
+
+  res.status(200).json({ text: text.replace(/\\n/, "").replace(/\\t/, "") });
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 export default function Homepage() {
+  const [url, setUrl] = useState("");
+  const [info, setInfo] = useState({
+    location: undefined,
+    text: "",
+  });
+  const { location, text } = info;
+
+  async function scan() {
+    const response = await fetch(`/api/scan?url=${encodeURIComponent(url)}`);
+    const data = await response.json();
+    setInfo(data);
+  }
+
   return (
-    <div className="mt-16 container mx-auto">
-      <h1>Hello world.</h1>
+    <div className="mt-16 container mx-auto flex items-center justify-center">
+      <div className="border rounded-xl p-5 border-gray-400 bg-gray-200 text-gray-900 flex flex-col gap-2">
+        <div>
+          <div className="text-xl">Enter a url to scan</div>
+          <input
+            className="outline-none py-1 px-2 text-center text-sm text-gray-700"
+            type="text"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button
+            onClick={scan}
+            className="bg-pink-500 text-white font-semibold shadow-md rounded py-1 px-5 flex items-center justify-center w-full text-sm">
+            Scan
+          </button>
+        </div>
+        {info.text}
+      </div>
     </div>
   );
 }
